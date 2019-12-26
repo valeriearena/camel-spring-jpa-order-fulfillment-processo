97f7a0ffeb2f2ffc11e1848b187332df07ad93c0
Valerie Arena <valeriearena@ITAdmins-MacBook-Pro.local>
2019-12-26 16:34:29 -0500

    refactored

--- src/main/java/com/pluralsight/orderfulfillment/routeBuilder/NewOrderRouteBuilder.java
+++ src/main/java/com/pluralsight/orderfulfillment/routeBuilder/NewOrderRouteBuilder.java
@@ -16,8 +16,8 @@ public class NewOrderRouteBuilder extends RouteBuilder {
     from("sql:" // from endpoint tells Camel where to get the data from the URI specified.
         + "select id from pluralsightorder where status = '"+ OrderStatus.NEW.getCode() + "'"
         + "?"
-        + "consumer.onConsume=update pluralsightorder set status = '" + OrderStatus.PROCESSING.getCode() + "' where id = :#id&consumer.delay=1000")
-    .beanRef("orderItemMessageTranslator", "transformToOrderItemMessage")
+        + "consumer.onConsume=update pluralsightorder set status = '" + OrderStatus.PROCESSING.getCode() + "' where id = :#id&consumer.delay=5000")
+    .beanRef("orderItemMessageTranslator", "transformToOrderItemMessage")  // Call a bean that's registered with camel and execute the method for translation.
     .to("activemq:queue:ORDER_ITEM_PROCESSING"); // to endpoint tells Camel to route the message to the URI specified.
   }
 
